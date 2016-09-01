9a16329476b2a0b432d377acd3a207f45957f4ca
Colby Crouch <colbycrouch@gmail.com>
2016-08-31 19:35:13 -0700

    i need to seek help here, but got the three divs separated but now empty

--- app.js
+++ app.js
@@ -1,89 +1,88 @@
 var randomPictures = [];
-var totalClicks = [];
-var click = document.getElementById('show-image');
+// var timesClicked = [];
 
-var addRt = document.getElementById('add-right');
-var addCntr = document.getElementById('add-center');
-var addLft = document.getElementById('add-left');
 var showImage = document.getElementById('show-image');
 
 function Picture(imgName, filePath) {
-  this.imgName = imgName;
-  this.filePath = filePath;
-  this.newPath = 'img/' + this.filePath + '.jpg alt="' + this.imgName + '" />';
-  this.timesShown = 0;
+  this.imgName = imgName,
+  this.filePath = filePath,
+  this.newPath = 'img/' + filePath,
+  this.width = 300,
+  this.height = 300,
+  this.timesShown = 0,
   this.timesClicked = 0;
+}
+randomPictures.push(new Picture('bag', 'bag.jpg'));
+randomPictures.push(new Picture('banana', 'banana.jpg'));
+randomPictures.push(new Picture('bathroom', 'bathroom.jpg'));
+randomPictures.push(new Picture('boots', 'boots.jpg'));
+randomPictures.push(new Picture('breakfast', 'breakfast.jpg'));
+randomPictures.push(new Picture('bubblegum', 'bubblegum.jpg'));
+randomPictures.push(new Picture('chair', 'chair.jpg'));
+randomPictures.push(new Picture('cthulhu', 'cthulhu.jpg'));
+randomPictures.push(new Picture('dog-duck', 'dog-duck.jpg'));
+randomPictures.push(new Picture('dragon', 'dragon.jpg'));
+randomPictures.push(new Picture('pen', 'pen.jpg'));
+randomPictures.push(new Picture('pet-sweep', 'pet-sweep.jpg'));
+randomPictures.push(new Picture('scissors', 'scissors.jpg'));
+randomPictures.push(new Picture('shark', 'shark.jpg'));
+randomPictures.push(new Picture('sweep', 'sweep.png'));
+randomPictures.push(new Picture('tauntaun', 'tauntaun.jpg'));
+randomPictures.push(new Picture('unicorn', 'unicorn.jpg'));
+randomPictures.push(new Picture('usb', 'usb.gif'));
+randomPictures.push(new Picture('water-can', 'water-can.jpg'));
+randomPictures.push(new Picture('wine-glass', 'wine-glass.jpg'));
 
-  randomPictures.push(new Picture('bag', 'bag'));
-  randomPictures.push(new Picture('banana', 'banana'));
-  randomPictures.push(new Picture('bathroom', 'bathroom'));
-  randomPictures.push(new Picture('boots', 'boots'));
-  randomPictures.push(new Picture('breakfast', 'breakfast'));
-  randomPictures.push(new Picture('bubblegum', 'bubblegum'));
-  randomPictures.push(new Picture('chair', 'chair'));
-  randomPictures.push(new Picture('cthulhu', 'cthulhu'));
-  randomPictures.push(new Picture('dog-duck', 'dog-duck'));
-  randomPictures.push(new Picture('dragon', 'dragon'));
-  randomPictures.push(new Picture('pen', 'pen'));
-  randomPictures.push(new Picture('pet-sweep', 'pet-sweep'));
-  randomPictures.push(new Picture('scissors', 'scissors'));
-  randomPictures.push(new Picture('shark', 'shark'));
-  randomPictures.push(new Picture('sweep', 'sweep'));
-  randomPictures.push(new Picture('tauntaun', 'tauntaun'));
-  randomPictures.push(new Picture('unicorn', 'unicorn'));
-  randomPictures.push(new Picture('usb', 'usb'));
-  randomPictures.push(new Picture('water-can', 'water-can'));
-  randomPictures.push(new Picture('wine-glass', 'wine-glass'));
+//function that runs math random to generate a random img from randomPictures array
+function calculateMath() {
+  return Math.floor(Math.random() * randomPictures.length);
+}
 
-  function runMath() {
-    return Math.floor(Math.random() * randomPictures.length);
+//function that handles imgs and keeps them from duplicating
+function genThree() {
+  var img1 = calculateMath();
+
+  var divEl = document.createElement('divEl');
+  divEl.innerHTML = '<img src=' + randomPictures[img1].newPath + '>';
+  randomPictures[img1].timesShown++;
+  showImage.appendChild(divEl);
+
+  var img2 = calculateMath(Picture);
+
+  while (img1 === img2) {
+    img2 = calculateMath();
   }
 
-  function generateThree() {
-    var img1 = runMath();
+  var divElTwo = document.createElement('divElTwo');
+  divElTwo.innerHTML = '<img src=' + randomPictures[img2].newPath + '>';
+  randomPictures[img2].timesShown++;
+  showImage.appendChild(divElTwo);
 
-    var divEl = document.createElement('div');
-    divEl.innerHTML = '<img src=' + randomPictures[img1].newPath;
-    randomPictures[img1].timesShown++;
-    showImage.appendChild(divEl);
+  var img3 = calculateMath();
 
-    var img2 = runMath(Picture);
-
-    while (img1 === img2) {
-      img2 = runMath();
-    }
-
-    var divElTwo = document.createElement('div');
-    divElTwo.innerHTML = '<img src=' + randomPictures[img2].newPath;
-    randomPictures[img2].timesShown++;
-    showImage.appendChild(divEl2);
-
-    var img3 = runMath();
-
-    while (img3 === img1 || img3 === img2) {
-      img3 = runMath();
-    }
-
-    var divElThree = document.createElement('div');
-    divElThree.innerHTML = '<img src=' + randomPictures[img3].newPath;
-    randomPictures[img3].timesShown++;
-    showImage.appendChild(divEl3);
+  while (img3 === img1 || img3 === img2) {
+    img3 = calculateMath();
   }
 
-  function handleClick(event) {
-    console.log('you done clicked on ' + event.target.alt);
+  var divElThree = document.createElement('divElThree');
+  divElThree.innerHTML = '<img src=' + randomPictures[img3].newPath + '>';
+  randomPictures[img3].timesShown++;
+  showImage.appendChild(divElThree);
+};
 
-    for (var q = 0; q < randomPictures.length; q++) {
-      if (event.target.alt === randomPictures[q].imgName) {
-        randomPictures[q].timesClicked++;
-      }
-      showImg.innerHTML = ' ';
-
-      generateThree();
+function handleClick(event) {
+  console.log('you have clicked here');
+  for (var q = 0; q < randomPictures.length; q++) {
+    if (event.target.alt === randomPictures[q].imgName) {
+      randomPictures[q].timesClicked++;
     }
+    showImg.innerHTML = ' ';
+
+    genThree();
   }
+};
 
-  newClick.addEventListener('click', handleClick);
+handleClick.addEventListener('click', handleClick);
 
-  generateThree();
-  handleClick();
+genThree();
+handleClick();
